582c619f9dfc2e643aee11e34a13868776c9c4ad
Mounir El-Abbas <eamounir1@gmail.com>
2021-11-15 12:37:42 -0500

    Add Images

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,10 +1,39 @@
+import HTMLImg from "./images/html.PNG";
+import CSSImg from "./images/css.PNG";
+import javascriptImg from "./images/javascript.PNG";
+import reactImg from "./images/react.PNG";
+import reduxImg from "./images/redux.PNG";
+import typescriptImg from "./images/typescript.PNG";
+import sassImg from "./images/sass.PNG";
+import gitImg from "./images/git.PNG";
+import nodeImg from "./images/node.PNG";
+import expressImg from "./images/express.PNG";
+import mongoDBImg from "./images/mongoDB.PNG";
+
 export { GiHamburgerMenu } from "react-icons/gi";
 export { FaRedhat } from "react-icons/fa";
 export { AiFillGithub } from "react-icons/ai";
 export { FaLinkedin } from "react-icons/fa";
-export { GrDocumentPdf } from "react-icons/gr";
+export { VscFilePdf } from "react-icons/vsc";
 export { AiOutlineHome } from "react-icons/ai";
 export { IoBarChartOutline } from "react-icons/io5";
-export { FcMultipleDevices } from "react-icons/fc";
+export { VscCode } from "react-icons/vsc";
 export { BsPerson } from "react-icons/bs";
 export { FiMail } from "react-icons/fi";
+export { FaChevronRight } from "react-icons/fa";
+export { SiElasticstack } from "react-icons/si";
+export { ImCross } from "react-icons/im";
+
+export {
+  HTMLImg,
+  CSSImg,
+  javascriptImg,
+  reactImg,
+  reduxImg,
+  typescriptImg,
+  sassImg,
+  gitImg,
+  nodeImg,
+  expressImg,
+  mongoDBImg,
+};
