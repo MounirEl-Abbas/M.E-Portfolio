868407bd6b062b93b4774f2cc0c3bf39c0374113
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-06 14:10:08 -0500

    Style Navbar

--- src/components/navbar/partials/NavHeader.js
+++ src/components/navbar/partials/NavHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavHeader = () => {
   return (
-    <header className="nav-header">
+    <header className="navbar-header">
       <h3>Mounir El-Abbas</h3>
       <p>Front-End Developer</p>
     </header>
