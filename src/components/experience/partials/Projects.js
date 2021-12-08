60f2c3bdf3d8f9fc3ca5b09c48c6de0521b62560
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-08 13:12:29 -0500

    Projects section macro-styling complete

--- src/components/experience/partials/Projects.js
+++ src/components/experience/partials/Projects.js
@@ -1,12 +1,42 @@
+import { MdOpenInNew } from "../../../assets";
+import placeholderImg from "../../../assets/images/hero-bg2.jpg";
 const Projects = () => {
   return (
     <section className="projects">
       <header>
         <h2>Projects</h2>
       </header>
-      <article>Project 1</article>
-      <article>Project 2</article>
-      <article>Project 3</article>
+      <div className="projects-container">
+        <article>
+          <figure>
+            <img src={placeholderImg} alt="" />
+          </figure>
+          <div>
+            <h4>Travel Website</h4>
+            <button>Learn More</button>
+          </div>
+        </article>
+        <article>
+          <figure>
+            <img src={placeholderImg} alt="" />
+          </figure>
+          <div>
+            <h4>
+              Memories <span>MERN</span>
+            </h4>
+            <button>Learn More</button>
+          </div>
+        </article>
+        <article>
+          <figure>
+            <img src={placeholderImg} alt="" />
+          </figure>
+          <div>
+            <h4>Project 3</h4>
+            <button>Learn More</button>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
