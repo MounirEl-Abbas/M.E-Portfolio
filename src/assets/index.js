182b61dcbdf75cc391c746bb625e6f5a65af8b6b
Mounir El-Abbas <eamounir1@gmail.com>
2022-01-28 14:43:10 -0500

    Changed some icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,13 @@
 //Icons
-export { GiHamburgerMenu } from "react-icons/gi";
-export { AiFillGithub } from "react-icons/ai";
+export { AiFillGithub, AiOutlineMenu } from "react-icons/ai";
 export {
-  FaLinkedin,
+  FaLinkedinIn,
   FaChevronLeft,
   FaChevronRight,
   FaArrowCircleUp,
 } from "react-icons/fa";
 export { VscFilePdf } from "react-icons/vsc";
-export { FiMail } from "react-icons/fi";
+export { IoMdMail } from "react-icons/io";
 export { ImCross } from "react-icons/im";
 export { MdOpenInNew } from "react-icons/md";
 export { HiDownload } from "react-icons/hi";
