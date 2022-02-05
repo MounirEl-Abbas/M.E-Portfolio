97729049cfd510a17debf8d5a1436730a24670d9
Mounir El-Abbas <eamounir1@gmail.com>
2022-02-05 12:13:25 -0500

    Component/Code-Cleanup

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
+
+//Info
 import { projects } from "../myInfo";
+//Icons
 import { FaChevronLeft, FaChevronRight } from "../assets";
+
 const ProjectSlider = ({ projectID }) => {
+  /* 3 Projects total, each state is store in one of the following */
   const [projectOneIndex, setProjectOneIndex] = useState(0);
   const [projectTwoIndex, setProjectTwoIndex] = useState(0);
   const [projectThreeIndex, setProjectThreeIndex] = useState(0);
 
   useEffect(() => {
+    /* Get total images amount for each project */
     const projectOneLastIndex = projects[0].images.length - 1;
+    /* Handle loop of images going backwards */
     if (projectOneIndex < 0) setProjectOneIndex(projectOneLastIndex);
+    /* Handle loop of images going forwards */
     if (projectOneIndex > projectOneLastIndex) setProjectOneIndex(0);
 
     const projectTwoLastIndex = projects[1].images.length - 1;
@@ -44,29 +52,36 @@ const ProjectSlider = ({ projectID }) => {
       {projects.map(project => {
         if (project.id === projectID) {
           return project.images.map((image, imageIndex) => {
+            /* Set up index-tracking for each project */
             let index;
             if (project.id === 1) index = projectOneIndex;
             if (project.id === 2) index = projectTwoIndex;
             if (project.id === 3) index = projectThreeIndex;
 
+            /* Default position */
             let position = "otherImage";
+            /* Filter next image */
             if (
               imageIndex === index + 1 ||
               (index === project.images.length - 1 && imageIndex === 0)
             )
               position = "nextImage";
-
+            /* Find active image */
             if (imageIndex === index) position = "activeImage";
 
             return (
               <figure className={position} key={imageIndex}>
-                <img src={image} alt="" />
+                <img
+                  src={image}
+                  alt="Image taken from this project's live demo"
+                />
               </figure>
             );
           });
         }
         return <span style={{ display: "none" }} key={project.id}></span>;
       })}
+      {/* Next/prev buttons */}
       <div className="slider-btns">
         <button
           className="prev"
