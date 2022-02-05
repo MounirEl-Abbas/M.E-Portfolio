1c6c0b50f4390e4077850df0c4287a174b140f4f
Mounir El-Abbas <eamounir1@gmail.com>
2022-02-05 11:57:15 -0500

    Component/Code-Cleanup

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+
+//Info
 import { projects } from "..//myInfo";
-import { MdOpenInNew, AiFillGithub } from "../assets";
+//Components
 import ProjectSlider from "./ProjectSlider";
+//Icons
+import { MdOpenInNew, AiFillGithub } from "../assets";
 
 const ProjectCard = () => {
   return (
     <>
       {projects.map(project => (
         <article className="project-card" key={project.id}>
+          {/* Project-card image slider */}
           <ProjectSlider projectID={project.id} />
+          {/* Project-card Info */}
           <div className="project-content">
             <div className="project-card-info">
               <h3>{project.name}</h3>
               <h5>{project.subTitle}</h5>
               <p>{project.description}</p>
             </div>
+
+            {/* Project-card buttons */}
             <div id="btn-container">
               <a href={project.url} target="_blank" rel="noopener noreferrer">
                 <span>
