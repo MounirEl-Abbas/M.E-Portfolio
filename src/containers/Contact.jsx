d73708b99d2bb2ef9820da8d43ebce9ad4ffe092
Mounir El-Abbas <eamounir1@gmail.com>
2022-02-05 11:44:43 -0500

    Container/Contact-Code-Cleanup

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+
+//Components
 import SocialsIcons from "../components/SocialsIcons";
+//Info
 import { footerInfo } from "../myInfo";
+//Icons
 import { MdLocationOn } from "../assets";
+
 const Contact = () => {
   const { headshot, title, subtitle, role, location } = footerInfo;
   return (
     <footer className="contact">
       <div className="contact-content">
         <figure>
-          <img src={headshot} alt="" />
+          <img src={headshot} alt="Photo of Mounir El-Abbas" />
         </figure>
         <div className="contact-info">
           <h3>{title}</h3>
