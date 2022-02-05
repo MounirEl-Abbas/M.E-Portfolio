2b1051a1e258a21bbe7dd5e5206dfb8ccd45f59f
Mounir El-Abbas <eamounir1@gmail.com>
2022-02-05 11:45:20 -0500

    Container/Education-Code-Cleanup

--- src/containers/Education.jsx
+++ src/containers/Education.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+
+//Components
 import EducationCard from "../components/EducationCard";
+//Icons
 import { ImBooks } from "../assets";
+
 const Education = () => {
   return (
     <section className="education">
